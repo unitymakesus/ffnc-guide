9b36d21f77b176ad7ccec4db973acc100907dbac
Alisa Herr <alisa@unitymakes.us>
2019-02-12 19:03:39 -0500

    Set up theme and plugins

---
+++ wp-content/plugins/bb-plugin/modules/content-slider/includes/frontend.css.php
@@ -0,0 +1,193 @@
+.fl-node-<?php echo $id; ?> .fl-content-slider,
+.fl-node-<?php echo $id; ?> .fl-slide {
+	min-height: <?php echo $settings->height; ?>px;
+}
+.fl-node-<?php echo $id; ?> .fl-slide-foreground {
+	margin: 0 auto;
+	max-width: <?php echo $settings->max_width; ?>px;
+}
+<?php
+if ( $settings->arrows ) :
+	if ( isset( $settings->arrows_bg_color ) && ! empty( $settings->arrows_bg_color ) ) :
+?>
+	.fl-node-<?php echo $id; ?> .fl-content-slider-svg-container {
+		background-color: <?php echo FLBuilderColor::hex_or_rgb( $settings->arrows_bg_color ); ?>;
+		width: 40px;
+		height: 40px;
+
+		<?php if ( isset( $settings->arrows_bg_style ) && 'circle' == $settings->arrows_bg_style ) : ?>
+		-webkit-border-radius: 50%;
+		-moz-border-radius: 50%;
+		-ms-border-radius: 50%;
+		-o-border-radius: 50%;
+		border-radius: 50%;
+		<?php endif; ?>
+	}
+	.fl-node-<?php echo $id; ?> .fl-content-slider-navigation svg {
+		height: 100%;
+		width: 100%;
+		padding: 5px;
+	}
+	<?php
+	endif;
+
+	if ( isset( $settings->arrows_text_color ) && ! empty( $settings->arrows_text_color ) ) :
+	?>
+	.fl-node-<?php echo $id; ?> .fl-content-slider-navigation path {
+		fill: <?php echo FLBuilderColor::hex_or_rgb( $settings->arrows_text_color ); ?>;
+	}
+	<?php
+	endif;
+endif;
+
+for ( $i = 0; $i < count( $settings->slides ); $i++ ) {
+
+	// Make sure we have a slide.
+	if ( ! is_object( $settings->slides[ $i ] ) ) {
+		continue;
+	}
+
+	// Slide Settings
+	$slide = $settings->slides[ $i ];
+
+	// Slide Background Color
+	if ( 'color' == $slide->bg_layout && ! empty( $slide->bg_color ) ) {
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i;
+		echo ' { background-color: ' . FLBuilderColor::hex_or_rgb( $slide->bg_color ) . '; }';
+	}
+
+	// Foreground Photo/Video
+	if ( 'photo' == $slide->content_layout || 'video' == $slide->content_layout ) {
+
+		$photo_width = 100 - $slide->text_width;
+
+		// Foreground Photo/Video Width
+		if ( 'center' != $slide->text_position ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-photo-wrap ';
+			echo '{ width: ' . $photo_width . '%; }';
+		}
+
+		// Foreground Photo/Video Margins
+		if ( 'left' == $slide->text_position ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-photo ';
+			echo '{ margin-right: ' . $slide->text_margin_left . 'px; ';
+			echo 'margin-top: ' . $slide->text_margin_top . 'px; ';
+			echo 'margin-bottom: ' . $slide->text_margin_bottom . 'px; }';
+		} elseif ( 'center' == $slide->text_position ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-photo ';
+			echo '{ margin-left: ' . $slide->text_margin_left . 'px; ';
+			echo 'margin-right: ' . $slide->text_margin_right . 'px; ';
+			echo 'margin-bottom: ' . $slide->text_margin_bottom . 'px; }';
+		} elseif ( 'right' == $slide->text_position ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-photo ';
+			echo '{ margin-left: ' . $slide->text_margin_right . 'px; ';
+			echo 'margin-top: ' . $slide->text_margin_top . 'px; ';
+			echo 'margin-bottom: ' . $slide->text_margin_bottom . 'px; }';
+		}
+	}
+
+	// Text Width and Margins
+	if ( 'none' != $slide->content_layout ) {
+
+		// Content wrap width
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content-wrap ';
+		echo '{ width: ' . $slide->text_width . '%; }';
+
+		// Margins
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content ';
+		echo '{ margin-right: ' . $slide->text_margin_right . 'px; ';
+		echo 'margin-left: ' . $slide->text_margin_left . 'px; ';
+
+		// 100% height, don't use top/bottom margins
+		if ( '100%' == $slide->text_bg_height && ! empty( $slide->text_bg_color ) ) {
+
+			// Content height
+			echo ' min-height: ' . $settings->height . 'px; }';
+
+			// Content wrap height
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content-wrap ';
+			echo '{ min-height: ' . $settings->height . 'px; }';
+		} else {
+			echo 'margin-top: ' . $slide->text_margin_top . 'px; ';
+			echo 'margin-bottom: ' . $slide->text_margin_bottom . 'px; }';
+		}
+	}
+
+	// Text Styles
+	if ( 'custom' == $slide->title_size ) {
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title ';
+		echo '{ font-size: ' . $slide->title_custom_size . 'px; }';
+	}
+
+	// Text Color
+	if ( ! empty( $slide->text_color ) ) {
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title, ';
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text, ';
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text * ';
+		echo '{ color: ' . FLBuilderColor::hex_or_rgb( $slide->text_color ) . '; }';
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text strong ';
+		echo '{ color: inherit; }';
+	}
+
+	// Text BG Color
+	if ( ! empty( $slide->text_bg_color ) ) {
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content ';
+		echo '{ background-color: ' . FLBuilderColor::hex_or_rgb( $slide->text_bg_color ) . ';';
+		echo 'padding-top: ' . $slide->text_padding_top . 'px;';
+		echo 'padding-right: ' . $slide->text_padding_right . 'px;';
+		echo 'padding-bottom: ' . $slide->text_padding_bottom . 'px;';
+		echo 'padding-left: ' . $slide->text_padding_left . 'px;}';
+	}
+
+	// Text Shadow
+	if ( $slide->text_shadow ) {
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title, ';
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text ';
+		echo '{ text-shadow: 0 0 5px rgba(0,0,0,0.3); }';
+	}
+
+	// Responsive Text Styles
+	if ( $global_settings->responsive_enabled ) {
+		echo '@media (max-width: ' . $global_settings->responsive_breakpoint . 'px) { ';
+
+		// Responsive Text Color
+		if ( ! empty( $slide->r_text_color ) ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title, ';
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text, ';
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text * ';
+			echo '{ color: ' . FLBuilderColor::hex_or_rgb( $slide->r_text_color ) . '; }';
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text strong ';
+			echo '{ color: inherit; }';
+		} else {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title, ';
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text, ';
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text * ';
+			echo '{ color: inherit; }';
+		}
+
+		// Responsive Text BG Color
+		if ( ! empty( $slide->r_text_bg_color ) ) {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content ';
+			echo '{ background-color: ' . FLBuilderColor::hex_or_rgb( $slide->r_text_bg_color ) . '; }';
+		} else {
+			echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-content ';
+			echo '{ background-color: transparent; }';
+		}
+
+		// Responsive Text Shadow
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-title, ';
+		echo '.fl-node-' . $id . ' .fl-slide-' . $i . ' .fl-slide-text ';
+		echo '{ text-shadow: none; }';
+
+		echo ' }';
+	}
+
+	// Button Styles
+	if ( 'button' == $slide->cta_type ) {
+		FLBuilderCSS::typography_field_rule( array(
+			'settings'     => $slide,
+			'setting_name' => 'btn_typography',
+			'selector'     => ".fl-slide-$i .fl-slide-cta-button .fl-button-wrap",
+		) );
+	}
+}
--- wp-content/themes/unity-child/resources/functions.php
+++ wp-content/themes/unity-child/resources/functions.php
@@ -1,97 +1,164 @@
 <?php
 
-/**
- * Do not edit anything in this file unless you know what you're doing
- */
-
-use Roots\Sage\Config;
-use Roots\Sage\Container;
+namespace App;
 
 /**
- * Helper function for prettying up errors
- * @param string $message
- * @param string $subtitle
- * @param string $title
+ * Theme assets
  */
-$sage_error = function ($message, $subtitle = '', $title = '') {
-    $title = $title ?: __('Sage &rsaquo; Error', 'sage');
-    $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
-    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
-    wp_die($message, $title);
-};
+add_action('wp_enqueue_scripts', function () {
+  // Enqueue files for child theme (which include the core assets as imports)
+  wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+  wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+
+  // Set array of theme customizations for JS
+  wp_localize_script( 'sage/main.js', 'simple_options', array('fonts' => get_theme_mod('theme_fonts'), 'colors' => get_theme_mod('theme_color')) );
+}, 100);
 
 /**
- * Ensure compatible version of PHP is used
+ * REMOVE WP EMOJI
  */
-if (version_compare('7.1', phpversion(), '>=')) {
-    $sage_error(__('You must be using PHP 7.1 or greater.', 'sage'), __('Invalid PHP version', 'sage'));
+ remove_action('wp_head', 'print_emoji_detection_script', 7);
+ remove_action('wp_print_styles', 'print_emoji_styles');
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+/**
+ * Enable plugins to manage the document title
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+ */
+add_theme_support('title-tag');
+
+/**
+ * Register navigation menus
+ * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+ */
+register_nav_menus([
+    'primary_navigation' => __('Primary Navigation', 'sage'),
+    'social_links' => __('Social Links', 'sage')
+]);
+
+/**
+ * Enable post thumbnails
+ * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+ */
+add_theme_support('post-thumbnails');
+
+/**
+ * Enable HTML5 markup support
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+ */
+add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
+
+/**
+ * Enable selective refresh for widgets in customizer
+ * @link https://developer.wordpress.org/themes/advanced-topics/customizer-api/#theme-support-in-sidebars
+ */
+add_theme_support('customize-selective-refresh-widgets');
+
+/**
+* Add support for Gutenberg.
+*
+* @link https://wordpress.org/gutenberg/handbook/reference/theme-support/
+*/
+add_theme_support( 'align-wide' );
+add_theme_support( 'disable-custom-colors' );
+add_theme_support( 'wp-block-styles' );
+
+/**
+ * Enqueue editor styles for Gutenberg
+ */
+// function simple_editor_styles() {
+//   wp_enqueue_style( 'simple-gutenberg-style', asset_path('styles/main.css') );
+// }
+// add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\simple_editor_styles' );
+
+/**
+ * Add image quality
+ */
+add_filter('jpeg_quality', function($arg){return 100;});
+
+/**
+ * Enable logo uploader in customizer
+ */
+add_image_size('simple-logo', 200, 200, false);
+add_image_size('simple-logo-2x', 400, 400, false);
+add_theme_support('custom-logo', array(
+  'size' => 'simple-logo-2x'
+));
+
+/**
+ * Set image sizes
+ */
+update_option( 'thumbnail_size_w', 300 );
+update_option( 'thumbnail_size_h', 300 );
+update_option( 'thumbnail_crop', 1 );
+update_option( 'medium_size_w', 600 );
+update_option( 'medium_size_h', 600 );
+add_image_size('tiny-thumbnail', 80, 80, true);
+add_image_size('small-thumbnail', 150, 150, true);
+add_image_size('medium-square-thumbnail', 400, 400, true);
+
+
+add_filter( 'image_size_names_choose', function( $sizes ) {
+  return array_merge( $sizes, array(
+    'tiny-thumbnail' => __( 'Tiny Thumbnail' ),
+    'small-thumbnail' => __( 'Small Thumbnail' ),
+    'medium-square-thumbnail' => __( 'Medium Square Thumbnail' ),
+  ) );
+} );
+
+/**
+ * Register sidebars
+ */
+add_action('widgets_init', function () {
+  $config = [
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ];
+});
+
+/**
+ * Add case studies post type
+ */
+function add_post_type() {
+ $argsTeam = array(
+   'labels' => array(
+				'name' => 'Case Studies',
+				'singular_name' => 'Case Study',
+				'add_new' => 'Add New',
+				'add_new_item' => 'Add New Case Study',
+				'edit' => 'Edit',
+				'edit_item' => 'Edit Case Study',
+				'new_item' => 'New Case Study',
+				'view_item' => 'View Case Study',
+				'search_items' => 'Search Case Studies',
+				'not_found' =>  'Nothing found in the Database.',
+				'not_found_in_trash' => 'Nothing found in Trash',
+				'parent_item_colon' => ''
+   ),
+   'public' => true,
+   'exclude_from_search' => false,
+   'publicly_queryable' => true,
+   'show_ui' => true,
+   'show_in_nav_menus' => false,
+   'menu_position' => 20,
+   'menu_icon' => 'dashicons-megaphone',
+   'capability_type' => 'page',
+   'hierarchical' => false,
+   'supports' => array(
+     'title',
+     'editor',
+     'revisions',
+     'page-attributes',
+     'thumbnail'
+   ),
+   'has_archive' => false,
+   'rewrite' => array(
+     'slug' => 'case-study'
+   )
+ );
+ register_post_type( 'ff-case-study', $argsTeam );
 }
-
-/**
- * Ensure compatible version of WordPress is used
- */
-if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
-    $sage_error(__('You must be using WordPress 4.7.0 or greater.', 'sage'), __('Invalid WordPress version', 'sage'));
-}
-
-/**
- * Ensure dependencies are loaded
- */
-if (!class_exists('Roots\\Sage\\Container')) {
-    if (!file_exists($composer = __DIR__.'/../vendor/autoload.php')) {
-        $sage_error(
-            __('You must run <code>composer install</code> from the Sage directory.', 'sage'),
-            __('Autoloader not found.', 'sage')
-        );
-    }
-    require_once $composer;
-}
-
-/**
- * Sage required files
- *
- * The mapped array determines the code library included in your theme.
- * Add or remove files to the array as needed. Supports child theme overrides.
- */
-array_map(function ($file) use ($sage_error) {
-    $file = "../app/{$file}.php";
-    if (!locate_template($file, true, true)) {
-        $sage_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'sage'), $file), 'File not found');
-    }
-}, ['helpers', 'setup']);
-
-/**
- * Here's what's happening with these hooks:
- * 1. WordPress initially detects theme in themes/sage/resources
- * 2. Upon activation, we tell WordPress that the theme is actually in themes/sage/resources/views
- * 3. When we call get_template_directory() or get_template_directory_uri(), we point it back to themes/sage/resources
- *
- * We do this so that the Template Hierarchy will look in themes/sage/resources/views for core WordPress themes
- * But functions.php, style.css, and index.php are all still located in themes/sage/resources
- *
- * This is not compatible with the WordPress Customizer theme preview prior to theme activation
- *
- * get_template_directory()   -> /srv/www/example.com/current/web/app/themes/sage/resources
- * get_stylesheet_directory() -> /srv/www/example.com/current/web/app/themes/sage/resources
- * locate_template()
- * ├── STYLESHEETPATH         -> /srv/www/example.com/current/web/app/themes/sage/resources/views
- * └── TEMPLATEPATH           -> /srv/www/example.com/current/web/app/themes/sage/resources
- */
-array_map(
-    'add_filter',
-    ['theme_file_path', 'theme_file_uri', 'parent_theme_file_path', 'parent_theme_file_uri'],
-    array_fill(0, 4, 'dirname')
-);
-Container::getInstance()
-    ->bindIf('config', function () {
-        return new Config([
-            'assets' => require dirname(__DIR__).'/config/assets.php',
-            'theme' => require dirname(__DIR__).'/config/theme.php',
-            'view' => require dirname(__DIR__).'/config/view.php',
-        ]);
-    }, true);
-
-/**
- * Manually include parent theme's functions.php file
- */
-require_once( get_stylesheet_directory() . '/functions.php' );
+add_action( 'init', __NAMESPACE__.'\\add_post_type' );
