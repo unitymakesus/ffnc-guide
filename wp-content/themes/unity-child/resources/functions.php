e842ab0e8d0bfe9dc8ca2139bf8978e3774b1cc8
Andy Marino <andy@unitymakes.us>
2020-03-11 09:52:43 -0400

    fix theme php notices

--- wp-content/themes/unity-child/resources/functions.php
+++ wp-content/themes/unity-child/resources/functions.php
@@ -3,6 +3,19 @@
 namespace App;
 
 /**
+ * Helper function for prettying up errors
+ * @param string $message
+ * @param string $subtitle
+ * @param string $title
+ */
+$sage_error = function ($message, $subtitle = '', $title = '') {
+  $title = $title ?: __('Sage &rsaquo; Error', 'sage');
+  $footer = '<a href="https://roots.io/sage/docs/">roots.io/sage/docs/</a>';
+  $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
+  wp_die($message, $title);
+};
+
+/**
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
