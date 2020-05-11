a64bebaec803563658621d3b4e26629e0f27722a
Andy Marino <andy@unitywebagency.com>
2020-05-11 13:39:56 -0400

    update layout for 404 template, remove plugins

---
+++ wp-content/themes/unity-child/resources/views/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+  <div class="container" style="margin-bottom:3rem;">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
+  </div>
+@endsection
